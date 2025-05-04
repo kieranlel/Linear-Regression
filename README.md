a8147f534e14b16a9ee89ff4e9d8c779b86453fd
Kieran <u7282015@anu.edu.au>
2025-05-04 12:08:28 +1000

    initialising commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## About
+
+This project is an implementation of linear regression.
---
+++ linreg.py
@@ -0,0 +1,8 @@
+def lin_model(w,b,x):
+    pass
+
+def cost_fun(w,b,x_data,y_data):
+    pass
+        
+def grad_desc():
+    pass
