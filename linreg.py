f798a9b64204dea59d430de09892d8cb0c9cf7f7
Kieran <u7282015@anu.edu.au>
2025-05-09 19:50:25 +1000

    gradient descent edited to work with multiple features

--- linreg.py
+++ linreg.py
@@ -2,39 +2,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def model(w, b):
+
     def model_fun(x):
-        return w * x + b
+        return np.dot(w,x) + b
+    
     return model_fun
 
 def cost_fun(w, b, x_data, y_data):
+
     sum = 0
     m = x_data.shape[0]
     f = model(w,b)
+
     for i in range(m):
         sum += (f(x_data[i]) - y_data[i]) ** 2
     return sum/(2*m)
 
-def display_model_w_data(f, x_data, y_data):
-    xs = np.linspace(min(x_data), max(x_data))
-    ys = f(xs)
-    plt.plot(x_data, y_data, 'o', color='r', label='Data', )
-    plt.plot(xs, ys, color='b', label='Model')
-    plt.legend()
-    plt.show()  
+def display_model_w_data(f, x_data, y_data, n):
+
+    if n == 1:
+        f_out = [f(x) for x in x_data]
+        # xs = np.linspace(min(x_data), max(x_data))
+        # ys = f(xs)
+        plt.plot(x_data, y_data, 'o', color='r', label='Data', )
+        plt.plot(x_data, f_out, color='b', label='Model')
+        plt.legend()
+        plt.show()  
+
+    elif n == 2:
+        # 3D graph here
+        pass
+        
 
 
 def grad_desc(x_data, y_data, alpha, iters):
-    w,b = 0,0
+    w = np.array([0] * len(x_data[0]), dtype='float64')
+    b = 0
     f = model(w,b)
-    m = x_data.shape[0]
+    m = len(x_data)
 
     for _ in range(iters):
 
-        cost_dw = 0
-        for i in range(m):
-            cost_dw += (f(x_data[i]) - y_data[i]) * x_data[i]
-        cost_dw /= m
+        # Calculate gradient wrt w
+        cost_dw = np.array([0] * len(x_data[0]))
+        cost_dw = cost_dw.astype('float64')
+        for i in range(len(x_data[0])):
+            sum = 0
+            for j in range(m):
+                sum += (f(x_data[j]) - y_data[j]) * x_data[j,i]
+            sum /= m
+            cost_dw[i] = sum
 
+        # Calculate gradient wrt b
         cost_db = 0
         for i in range(m):
             cost_db += (f(x_data[i]) - y_data[i])
@@ -44,5 +63,5 @@ def grad_desc(x_data, y_data, alpha, iters):
         b -= alpha * cost_db
 
         f = model(w, b)
-
+        
     return f
